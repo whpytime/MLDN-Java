eda8883fa92aaac580f8e3cb769ce866eb561477
whpytime <whpytime@163.com>
2015-01-15 13:49:19 +0800

    Update at office.

---
+++ MLDN-Java/src/com/mldn/java/ninetyNine/IODemoOne.java
@@ -0,0 +1,19 @@
+package com.mldn.java.ninetyNine;
+
+import java.io.*;
+
+public class IODemoOne {
+
+	public static void main(String[] args) throws Exception {
+		File file = new File("d:" + File.separator + "testjava" + File.separator + "myinfo.txt");
+		if (!file.getParentFile().exists()) {
+			file.getParentFile().mkdirs();// 先创建文件和文件夹
+		}
+		OutputStream out = new FileOutputStream(file);// 向上转型
+		String str = "Hello, World!";// 字符串
+		byte[] data = str.getBytes();// 将字符串转换为字符数组
+		out.write(data);// 全部输出
+		out.close();// 关闭
+	}
+
+}
