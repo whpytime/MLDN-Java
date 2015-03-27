46a295985153e8d4b66ac89eb5a17d89659b4431
whpytime <whpytime@163.com>
2015-03-27 16:45:22 +0800

    update at office

---
+++ MLDN-Java/src/com/mldn/java/newClass/two/Hello.java
@@ -0,0 +1,22 @@
+package com.mldn.java.newClass.two;
+
+public class Hello{
+	public static void main(String args[])	{
+		A a = new A();
+		B b = new B();
+
+		System.out.println("Hello,world!");
+	}
+}
+
+class A{
+	A()	{
+		System.out.println("A");
+	}
+}
+
+class B{
+	B()	{
+		System.out.println("B");
+	}
+}
